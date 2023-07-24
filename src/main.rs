6616b48d1da10836f7b094bc14f1d942c1e3c703
swap-10 <swapnalv10@gmail.com>
2023-07-24 20:49:44 +0530

    Change to robust request-response handling, arbitrary file serving, configurable address and port

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,155 @@
-use std::{
-    fs,
-    io::{prelude::*, BufReader},
-    net::{TcpListener, TcpStream},
-};
-
+use std::io::prelude::*;
+use std::net::TcpListener;
+use std::net::TcpStream;
+use std::fs;
 use rustic_server::ThreadPool;
 
-fn main() {
-    let listener = TcpListener::bind("127.0.0.1:9393").unwrap();
-    let thread_pool = ThreadPool::new(5);
+struct Config {
+    address: String,
+    port: String
+}
 
-    for stream in listener.incoming() {
-        let stream_object = stream.unwrap();
-        
-        thread_pool.execute(|| {
-            handle_connection(stream_object);
-        });
+struct Request {
+    method: String,
+    path: String,
+    protocol: String
+}
+
+struct Response {
+    protocol: String,
+    status: String,
+    body: String
+}
+
+struct RequestHandler {
+    request: Request,
+    response: Response
+}
+
+// Implements the config
+impl Config {
+    // Create a new instance of Config
+    fn new() -> Config {
+        Config {
+            address: String::from(""),
+            port: String::from(""),
+        }
+    }
+
+    // Parse the command line arguments
+    fn parse_args(&mut self, args: &Vec<String>) {
+
+        self.address = args[1].clone();
+
+        self.port = args[2].clone();
     }
 }
 
-fn handle_connection(mut stream: TcpStream) {
-    let buf_reader = BufReader::new(&mut stream);
-    let request_line = buf_reader.lines().next().unwrap().unwrap();
+// Implements the RequestHandler
+impl RequestHandler {
+    // Creates a new instance of RequestHandler
+    fn new() -> RequestHandler {
+        RequestHandler {
+            request: Request {
+                method: String::from(""),
+                path: String::from(""),
+                protocol: String::from("")
+            },
+            response: Response {
+                protocol: String::from("HTTP/1.1"),
+                status: String::from("200 OK"),
+                body: String::from("")
+            }
+        }
+    }
 
-    let (status_line, filename) = match &request_line[..] {
-        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "../static/index.html"),
-        _ => ("HTTP/1.1 404 NOT FOUND", "../static/404.html")
-    };
-    let contents = fs::read_to_string(filename).unwrap();
-    let length = contents.len();
+    // parses the request string and populates the Request struct
+    fn parse_request(&mut self, request: &String) {
+        let mut lines = request.lines();
+        let mut line = lines.next().unwrap().split_whitespace();
 
-    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+
+        // Get the request method
+        self.request.method = String::from(line.next().unwrap());
+
+        // Get the request path and add the static folder path to it
+        let mut path = String::from("./static");
+        path.push_str(line.next().unwrap());
+        
+        if path.ends_with("/") {
+            path.pop();
+        }
+
+        self.request.path = path;
+
+        // Get the request protocol
+        self.request.protocol = String::from(line.next().unwrap());
+
+    }
+
     
-    stream.write_all(response.as_bytes()).unwrap();
+    // get the response, set the status and body    
+    fn get_response(&mut self) {
+
+        if self.request.method == "GET" {
+            // Check if the path exists
+            if fs::metadata(&self.request.path).is_ok() {
+                // Get the file contents
+                self.response.body = fs::read_to_string(&self.request.path).unwrap();
+            } else {
+                // Set the status to 404
+                self.response.status = String::from("404 NOT FOUND");
+
+                self.response.body = fs::read_to_string("./static/404.html").unwrap();
+            }
+        } else {
+
+            self.response.status = String::from("405 METHOD NOT ALLOWED");
+
+            self.response.body = fs::read_to_string("./static/404.html").unwrap();
+        }
+        
+        self.response.body = format!("{} {} {}\r\n\r\n{}", self.response.protocol, self.response.status, self.response.body.len(), self.response.body);
+
+    }
+}
+
+
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+
+    stream.read(&mut buffer).unwrap();
+
+    let mut request_handler = RequestHandler::new();
+    request_handler.parse_request(&String::from_utf8_lossy(&buffer[..]).to_string());
+
+    request_handler.get_response();
+
+    stream.write(request_handler.response.body.as_bytes()).unwrap();
+
+    // Flush the stream
+    stream.flush().unwrap();
+}
+
+
+fn main() {
+
+    let mut config = Config::new();
+
+    config.parse_args(&std::env::args().collect());
+
+    let thread_pool = ThreadPool::new(5);
+
+    // Create a new instance of TcpListener
+    let listener = TcpListener::bind(format!("{}:{}", config.address, config.port)).unwrap();
+
+    for stream in listener.incoming() {
+        // Get the stream
+        let stream_obj = stream.unwrap();
+
+        thread_pool.execute(|| {
+            handle_connection(stream_obj);
+        });
+    }
 }
